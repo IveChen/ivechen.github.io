386a09067848bfd1d3762cc314ad53a7d638437b
chenjiancai <844364762@qq.com>
2024-05-29 13:26:23 +0800

    增加版本时间

--- src/utils/commonConfig.ts
+++ src/utils/commonConfig.ts
@@ -4,13 +4,13 @@ export const shortcuts = [
   {
     text: '今年',
     value: () => {
-      const end = new Date()
       const start = new Date(new Date().getFullYear(), 0)
+      const end = new Date()
       return [start, end]
     }
   },
   {
-    text: '7.33以前',
+    text: '"改地图"以前',
     value: () => {
       const start = dayjs('2022-6-6')
       const end = dayjs('2023-4-20')
@@ -18,9 +18,33 @@ export const shortcuts = [
     }
   },
   {
-    text: '7.33以后',
+    text: '"改地图"以后',
     value: () => {
       const start = dayjs('2023-4-21')
+      const end = new Date()
+      return [start, end]
+    }
+  },
+  {
+    text: '"改地图"后 - "命石"前',
+    value: () => {
+      const start = dayjs('2023-4-21')
+      const end = dayjs('2024-5-23')
+      return [start, end]
+    }
+  },
+  {
+    text: '"命石"以前',
+    value: () => {
+      const start = dayjs('2022-6-6')
+      const end = dayjs('2024-5-23')
+      return [start, end]
+    }
+  },
+  {
+    text: '"命石"以后',
+    value: () => {
+      const start = dayjs('2024-5-24')
       const end = new Date()
       return [start, end]
     }

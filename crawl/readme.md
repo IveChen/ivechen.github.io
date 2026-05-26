6d391493ec29c9e5c1c053ad6bb733ad655b556a
chenjiancai <844364762@qq.com>
2026-05-26 09:46:22 +0800

    feat: 🎸 增加against_the_storm

--- crawl/readme.md
+++
@@ -1,2 +0,0 @@
-pip3 freeze > requirements.txt
-pip3 install -r requirements.txt
--- src/components/GobalMatchListFilter/globalFilteData.ts
+++
@@ -1,9 +0,0 @@
-import { reactive } from 'vue'
-
-export const state = reactive({
-  form: {
-    date: [],
-    matchType: ['TICU正赛', '对黑'],
-    excludePlayer: []
-  }
-})
